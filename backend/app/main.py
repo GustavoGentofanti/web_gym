8564a2e9a2a56d32fef096cd1233cae70874edaa
Gustavo Gentofanti <gustavogentofanti.pro@gmail.com>
2026-08-30 05:44:46 -0300

    Fix: cors_origins minúsculo

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[str(origin).rstrip("/") for origin in settings.CORS_ORIGINS],
+    allow_origins=[str(origin).rstrip("/") for origin in settings.cors_origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
